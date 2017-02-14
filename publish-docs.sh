3b9b97af8d1744a210abaa4da2b6fb42480d9f4c
Jaime Vega <jhuesos@gmail.com>
2017-02-14 08:52:22 +0100

    Copying publish docs script to repo, tweaked and added new npm run script for that.

---
+++ publish-docs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -e
+#
+# @license
+# Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+# This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+# The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+# The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+# Code distributed by Google as part of the polymer project is also
+# subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+#
+
+# This script pushes a demo-friendly version of your element and its
+# dependencies to gh-pages.
+
+# This script was copied from: https://github.com/Polymer/tools/blob/master/bin/gp.sh and tweaked with small changes.
+
+org="ellipticaljs"
+repo="paper-autocomplete"
+branch="master"
+# Folder where the documentation will be assembled
+folder=build/docs
+
+# make folder (same as input, no checking!)
+rm -fr $folder
+git clone git@github.com:$org/$repo.git --single-branch $folder
+
+# switch to gh-pages branch
+pushd $folder >/dev/null
+git checkout --orphan gh-pages
+
+# remove all content
+git rm -rf -q .
+
+# use bower to install runtime deployment
+bower cache clean $repo # ensure we're getting the latest from the desired branch.
+git show ${branch}:bower.json > bower.json
+echo "{
+  \"directory\": \"components\"
+}
+" > .bowerrc
+bower install
+bower install $org/$repo#$branch
+git checkout ${branch} -- demo
+rm -rf components/$repo/demo
+mv demo components/$repo/
+
+# redirect by default to the component folder
+echo "<META http-equiv="refresh" content=\"0;URL=components/$repo/\">" >index.html
+
+# send it all to github
+git add -A .
+git commit -am 'seed gh-pages'
+git push -u origin gh-pages --force
+
+popd >/dev/null
+
+# remove folder with documentation
+rm -fr $folder
