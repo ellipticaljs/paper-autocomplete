99741af27cbced3b2c6128ddef10ab436d89b9f8
Robert Klebe <robert.klebe@neovici.se>
2020-11-24 17:05:14 +0100

    fix: hide suggestions on clear

--- paper-autocomplete.js
+++ paper-autocomplete.js
@@ -515,12 +515,6 @@ Polymer({
 		if (!this.$.autocompleteInput.focused) {
 			this.$.autocompleteInput.focus();
 		}
-
-		this.$.paperAutocompleteSuggestions._handleSuggestions({
-			target: {
-				value: ''
-			}
-		});
 	},
 
 	/**
